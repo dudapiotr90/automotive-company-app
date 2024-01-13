2fa540227728f76261d9168c5f89022130eef82a
Piotr Duda <dudapiotr90@gmail.com>
2024-01-13 20:44:15 +0100

    design, employee, hr services initial setup

---
+++ hr-service/src/test/java/pl/dudi/hrservice/HrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dudi.hrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
