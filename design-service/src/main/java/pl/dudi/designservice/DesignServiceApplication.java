8c736e2ae67177ee34dd2616ff437738a664250b
Piotr Duda <dudapiotr90@gmail.com>
2024-01-13 20:44:15 +0100

    design, employee, hr services initial setup

---
+++ design-service/src/main/java/pl/dudi/designservice/DesignServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.dudi.designservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignServiceApplication.class, args);
+	}
+
+}
