d1cd1835b18e9dd3e199d7de87cee8c9b979b4e7
Piotr Duda <dudapiotr90@gmail.com>
2024-01-21 14:01:37 +0100

    FeignClient error decoder

---
+++ account-service/src/main/java/pl/dudi/accountservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package pl.dudi.accountservice.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(annotations = RestController.class)
+public class GlobalExceptionHandler {
+
+
+}
---
+++ customer-service/src/main/java/pl/dudi/clientservice/configuration/common/CommonBeanConfiguration.java
@@ -0,0 +1,42 @@
+package pl.dudi.clientservice.configuration.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import java.time.Clock;
+
+@Configuration
+public class CommonBeanConfiguration {
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(5));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(5));
+        return factory.createMultipartConfig();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .registerModule(new Jdk8Module())
+            .configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+
+}
