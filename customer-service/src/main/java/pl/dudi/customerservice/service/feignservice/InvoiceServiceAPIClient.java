254fa7ea121f9cea0b4b71df0959d08e9e77fb78
Piotr Duda <dudapiotr90@gmail.com>
2024-01-29 19:46:20 +0100

    invoice history call

---
+++ customer-service/src/main/java/pl/dudi/customerservice/service/feignservice/InvoiceServiceAPIClient.java
@@ -0,0 +1,21 @@
+package pl.dudi.customerservice.service.feignservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import pl.dudi.basedomains.dto.OrderDto;
+import pl.dudi.basedomains.dto.PageRequestDto;
+import pl.dudi.customerservice.configuration.feign.FeignClientConfig;
+import pl.dudi.customerservice.model.InvoiceDto;
+
+@FeignClient(name="invoice-service",configuration = FeignClientConfig.class)
+public interface InvoiceServiceAPIClient {
+
+    @GetMapping("/orders/{code}")
+    Page<OrderDto> getOrderHistory(@PathVariable(name = "code") int customerCode, @RequestBody PageRequestDto pageRequestDto);
+
+    @GetMapping("/invoices/{code}")
+    Page<InvoiceDto> getInvoiceHistory(@PathVariable(name = "code") int customerCode,@RequestBody PageRequestDto pageRequestDto);
+}
