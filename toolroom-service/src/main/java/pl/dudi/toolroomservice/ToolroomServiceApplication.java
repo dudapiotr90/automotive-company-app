6ad2e0763a6d30b65e94b31ec904bbc0b1faa7a9
Piotr Duda <dudapiotr90@gmail.com>
2024-02-03 13:07:21 +0100

    added toolroom service

---
+++ toolroom-service/src/main/java/pl/dudi/toolroomservice/ToolroomServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.dudi.toolroomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolroomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolroomServiceApplication.class, args);
+	}
+
+}
