a879f8d1a351548d9188a22bb3fe6d691520950a
Piotr Duda <dudapiotr90@gmail.com>
2024-02-03 13:07:21 +0100

    added toolroom service

---
+++ toolroom-service/src/test/java/pl/dudi/toolroomservice/ToolroomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dudi.toolroomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolroomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
