c45d9d98643f7057d109cd657799e6220d1c51d0
Piotr Duda <dudapiotr90@gmail.com>
2024-02-05 21:26:38 +0100

    invoice issue structure

--- management-service/src/main/java/pl/dudi/managementservice/service/producer/InvoiceProducer.java
+++ management-service/src/main/java/pl/dudi/managementservice/service/producer/InvoiceProducer.java
@@ -1,4 +1,27 @@
 package pl.dudi.managementservice.service.producer;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import pl.dudi.managementservice.dto.InvoiceDto;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class InvoiceProducer {
+
+    @Value("${rabbitmq.exchange.management.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.binding.management.invoice.routing.key}")
+    private String managementInvoiceRoutingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public InvoiceDto generateInvoice(String orderNumber) {
+
+        return null;
+    }
 }
